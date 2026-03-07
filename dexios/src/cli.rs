18fe465d9ec69b5a7e5d111b401ac26fb35a37f0
Alex V. <119082209+Retengart@users.noreply.github.com>
2026-03-08 00:17:21 +0300

    chore: update release metadata and branch defaults

--- dexios/src/cli.rs
+++ dexios/src/cli.rs
@@ -146,7 +146,7 @@ pub fn build_cli() -> Command {
 
     Command::new("dexios")
         .version(clap::crate_version!())
-        .author("brxken128 <brxken128@tutanota.com>")
+        .author(clap::crate_authors!("\n"))
         .about("Secure, fast and modern command-line encryption of files.")
         .subcommand_required(true)
         .arg_required_else_help(true)
